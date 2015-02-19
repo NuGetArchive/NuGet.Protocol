343e2193566a88f0f02c64019adc69c343791cfe
Maarten Balliauw <maarten@maartenballiauw.be>
2015-02-19 08:48:00 +0100

    Added resourceprovider for fetching V3 stats

---
+++ test/Client.V3Test/V3StatsResourceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Client;
+using NuGet.PackagingCore;
+using NuGet.Versioning;
+using Xunit;
+
+namespace Client.V3Test
+{
+    public class V3StatsResourceTests : TestBase
+    {
+        [Fact]
+        public async Task DownloadResource_Found()
+        {
+            //var resource = await SourceRepository.GetResourceAsync<V3StatsResource>();
+            var resource = new V3StatsResource(DataClient, new Uri("https://nugetgallery.blob.core.windows.net/v3-stats0/")); // todo: remove and replace with line above when the index.json has this service type listed
+            
+            var res = await resource.GetTotalStats(CancellationToken.None);
+
+            Assert.NotNull(res);
+        }
+    }
+}
