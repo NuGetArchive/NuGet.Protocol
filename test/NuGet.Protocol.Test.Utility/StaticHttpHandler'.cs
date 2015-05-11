92aef790a2f7b3730b602de052695b6f63f159ab
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-11 20:39:37 +0200

    Update license header on files.

--- test/NuGet.Protocol.Test.Utility/StaticHttpHandler'.cs
+++ test/NuGet.Protocol.Test.Utility/StaticHttpHandler'.cs
@@ -1,4 +1,6 @@
-﻿using NuGet.Configuration;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using NuGet.Configuration;
 using NuGet.Protocol.Core.Types;
 using System;
 using System.Collections.Generic;
--- test/NuGet.Protocol.Tests/ResolverMetadataClientTests.cs
+++ test/NuGet.Protocol.Tests/ResolverMetadataClientTests.cs
@@ -1,4 +1,6 @@
-﻿using NuGet.Protocol.Core.v3.DependencyInfo;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using NuGet.Protocol.Core.v3.DependencyInfo;
 using NuGet.Versioning;
 using Xunit;
 using Xunit.Extensions;
